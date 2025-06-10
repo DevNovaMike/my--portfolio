38dfd30b477ed149988274f2c2fc8ff23f61fc5f
DevNovaMike <michaelgraham565@gmail.com>
2025-06-10 08:58:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function sayHello() {
+  alert("Hello! Thanks for visiting 😊");
+}
+function sayHello() {
+  alert("Hello! Thanks for visiting 😊");
+}
+
+function greetUser(name) {
+  alert("Welcome, " + name + "! Glad you’re here.");
+}
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
